80dd0bcbdd9ad3012441e0a24fca439ce83fd9f8
Draeuss <147552949+Draeusss@users.noreply.github.com>
2024-12-28 17:43:07 +0100

    Delete Virus Risitas (version simplifiée et innofensive) directory

--- Virus Risitas (version simplifiée et innofensive)/compilation.bat
+++
@@ -1,1 +0,0 @@
-g++ -std=c++20 -static -static-libgcc -static-libstdc++ -O3 -DNDEBUG src/*.cpp -o output/"Twitch Booster" -lole32 -lsapi -luuid -lwinmm -lws2_32 -lcomctl32 -lcomdlg32 -limm32 -lversion -lshlwapi -lsetupapi -lwininet -lurlmon -lshell32 -luser32 -lgdi32 -ladvapi32 -lkernel32 -lntdll -lpsapi -liphlpapi
